8ddc05cf49dc18a6298f51dbb7cd23ee9407e789
Jonathan Anderson <janderson@janderson.ca>
2026-09-07 10:45:10 -0400

    Use netlink.Wireguard instead of local link shim
    vishvananda/netlink#464 merged; the shim in wg/netlink.go is redundant.

--- wg/wireguard.go
+++ wg/wireguard.go
@@ -103,7 +103,7 @@ func (s *State) DownInterface() error {
 
 // SetUpInterface creates and sets up the associated network interface.
 func (s *State) SetUpInterface(nodes []common.Node) error {
-	if err := netlink.LinkAdd(&wireguard{LinkAttrs: netlink.LinkAttrs{Name: s.iface}}); err != nil && !os.IsExist(err) {
+	if err := netlink.LinkAdd(&netlink.Wireguard{LinkAttrs: netlink.LinkAttrs{Name: s.iface}}); err != nil && !os.IsExist(err) {
 		return fmt.Errorf("creating link %s: %w", s.iface, err)
 	}
 
